4ec926b01c70edc8ff42344b6300fe347ccce20c
Bryan Joshua Pedini <b.pedini@bjphoster.com>
2022-03-16 13:01:16 +0100

    added server first setup script

---
+++ webroot/server-first-setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e
+
+# Install git, python3 and ansible
+apt update
+apt install -y git python3 python3-pip
+pip3 install ansible
+
+git clone https://git.bjphoster.com/b.pedini/server-setup.git
+cd server-setup
+ansible-playbook main.yml
